43f207eed9922b2fc6edd01b39cb0bf1a9379fa0
s-evangelistaj <130245284+s-evangelistaj@users.noreply.github.com>
2023-05-13 22:34:48 -0700

    Initial Push

--- artifact03/script.js
+++ artifact03/script.js
@@ -40,17 +40,34 @@ function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
 }
 
 function upLeft(pHeight, pColorEven, pColorOdd, pSymbol){
-  var rLine="";
-  for (i=0;i>pHeight;i++){
-    rLine +="<p>";
+  var lLine="";
+  for (i=0;i<pHeight;i++){
+    lLine +="<p>";
     for (j=0;j<=i;j++){
       if (j%2)
-        rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+        lLine +="<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
       else
-        rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+        lLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
        
     }
-    rLine += "</p>";
+    lLine += "</p>";
   }
-  document.getElementById("upLeft").innerHTML = rLine;
+  document.getElementById("upLeft").innerHTML = lLine;
+}
+
+function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+  var lLine ="";
+  for (i=pHeight;i>0;i--){
+    lLine +="<p>";
+    for (j=0;j<i;j++){
+      if (j%2)
+        lLine +="<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+      else
+        lLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+      
+    }
+    lLine += "</p>";
+  }
+  
+  document.getElementById("downLeft").innerHTML = lLine;
 }
