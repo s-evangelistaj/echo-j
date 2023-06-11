42c528d15af3aca21529b68af68034fa2b4643a6
s-evangelistaj <130245284+s-evangelistaj@users.noreply.github.com>
2023-06-11 16:03:01 -0700

    Initial Push

--- artifact05/script.js
+++ artifact05/script.js
@@ -1,1 +1,44 @@
+var slideNumber = 1;
+var automatic = null;
+showSlide(slideNumber);
+	
+function nextSlide(n) {
+	clearInterval(automatic);
+	showSlide(slideNumber += n);
+}
+
+function currentSlide(n) {
+	clearInterval(automatic);
+	showSlide(slideNumber = n);
+}
+
+function autoSlide() { 
+	clearInterval(automatic);
+	
+	let i;
+	let slides = document.getElementsByClassName("Slides");
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	slideNumber++;
+	if (slideNumber > slides.length) {slideNumber = 1}
+	slides[slideNumber-1].style.display = "block";
+	automatic = setInterval(autoSlide, 2000);
+}
+
+function showSlide(n) {
+	let i;
+	let slides = document.getElementsByClassName("Slides");
+	if (n > slides.length) {slideNumber = 1}
+	if (n < 1) {slideNumber = slides.length}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	slides[slideNumber-1].style.display = "block";
+}
+
+	
+
+
+
 
